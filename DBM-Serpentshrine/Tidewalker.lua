6528834eb67c3ac55803486486866e1d7f4af1d0
fxpw <fxpw@outlook.com>
2022-08-11 23:35:06 +0300

    ренджа

--- DBM-Serpentshrine/Tidewalker.lua
+++ DBM-Serpentshrine/Tidewalker.lua
@@ -135,7 +135,7 @@ function mod:SPELL_CAST_START(args)
 		timerZemlaCast:Start()
 		timerZemlaCD:Start()
 		specWarnZemla:Show()
-		DBM.RangeCheck:Show(6)
+		DBM.RangeCheck:Show(8)
 	elseif spellId == 310151 then -- Землетрясение
 		specWarnKrik:Show()
 		warnKrik:Show()
