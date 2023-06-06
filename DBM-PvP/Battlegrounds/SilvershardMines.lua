8e5361ce0baf9079835de32b7a2e7be3bcc44a0d
fxpw <fxpw@outlook.com>
2023-06-06 17:36:28 +0300

    clear

--- DBM-PvP/Battlegrounds/SilvershardMines.lua
+++ DBM-PvP/Battlegrounds/SilvershardMines.lua
@@ -46,7 +46,6 @@ local cartTimer	= mod:NewTimer(14.5, "TimerCart", "Interface\\Icons\\Spell_Frost
 local battleStart = false
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
-	-- print(msg)
 	if not bgzone or not msg then
 		return
 	end
@@ -92,149 +91,7 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	end
 	-- mod:UPDATE_WORLD_STATES()
 end
-mod.CHAT_MSG_SYSTEM = mod.CHAT_MSG_RAID_BOSS_EMOTE
-mod.CHAT_MSG_BG_SYSTEM_ALLIANCE = mod.CHAT_MSG_RAID_BOSS_EMOTE
-mod.CHAT_MSG_BG_SYSTEM_HORDE = mod.CHAT_MSG_RAID_BOSS_EMOTE
-mod.CHAT_MSG_BG_SYSTEM_NEUTRAL = mod.CHAT_MSG_RAID_BOSS_EMOTE
 
-
-
-local function IsValidUpdate(dir1, dir2)
-	if dir1 == 0 or dir2 == 0 then
-		return false
-	end
-	if dir1 == dir2 then
-		return true
-	elseif dir1 == 3 or dir2 == 3 then
-		return false
-	elseif abs(dir1 - dir2) == 1 then
-		return true
-	end
-	return false
-end
-
-local function GetDistance(x1, y1, x2, y2)
-	return sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
-end
-
--- function ClearCartCache()
--- 	local time = GetTime()
--- 	for i, cart in pairs(carts) do
--- 		if cart.dir ~= 0 and times[cart.dir] + cart.spawn + 2 > time then
--- 			if GetDistance(cart.x, cart.y, caps[cart.dir].x, caps[cart.dir].y) < 3 then
--- 				cartTimer:Stop(names[cart.dir])
--- 				tremove(carts, i)
--- 			end
--- 		end
--- 	end
--- end
-
--- local function PointToLineDist(a, b, x, y)
--- 	return (a * x + b - y) / (sqrt(a ^ 2 + 1))
--- end
-
--- local function IdentifyCartCoord(x, y)
--- 	local dist1, dist2, dist3, dist4 = PointToLineDist(-2.126, -168.449, x, y), PointToLineDist(-0.513, 76.476, x, y), PointToLineDist(-0.555, 64.673, x, y), PointToLineDist(0.952, -12.176, x, y)
--- 	if dist1 < 0 and dist3 < 0 then
--- 		return dist4 > 0 and 5 or 4 -- Lava Up / Down
--- 	elseif dist1 > 0 and dist3 < 0 and dist2 < 0 then
--- 		return 3 -- Middle
--- 	elseif dist2 > 0 and (dist1 > 0 or dist3 > 0) then
--- 		return dist4 > 0 and 2 or 1 -- Top Up / Down
--- 	end
--- end
-
--- local function IdentifyCart(cartNum)
--- 	local cart = carts[cartNum]
--- 	if not cart then
--- 		return
--- 	end
--- 	local closestID, distance = 0, 1000
--- 	for d = 1, GetNumBattlefieldVehicles() do
--- 		local x, y = GetBattlefieldVehicleInfo(d)
--- 		x = x * 100
--- 		y = y * 100
--- 		local dist = GetDistance(56.87, 47.117, x, y)
--- 		if dist < distance then
--- 			local used = false
--- 			for _, v in pairs(carts) do
--- 				if GetDistance(x, y, v.x, v.y) < 2 then
--- 					used = true
--- 					break
--- 				end
--- 			end
--- 			if not used then
--- 				closestID = d
--- 				distance = dist
--- 			end
--- 		end
--- 	end
--- 	if closestID ~= 0 then
--- 		local x, y = GetBattlefieldVehicleInfo(closestID)
--- 		cart.x		= x * 100
--- 		cart.y		= y * 100
--- 		cart.dir	= IdentifyCartCoord(cart.x, cart.y)
--- 	end
--- end
--- local cache = {}
--- function mod:UPDATE_WORLD_STATES()
--- 	if not bgzone then return end
--- 	table.wipe(cache)
--- 	for i = 1, GetNumBattlefieldVehicles() do
--- 		local x, y, nameRu , _ , name  =  GetBattlefieldVehicleInfo(i)
--- 		if x and y and name then
--- 			local index = string.match(nameRu,"1") and 1 or
--- 			string.match(nameRu,"2") and 2 or
--- 			string.match(nameRu,"3") and 3
--- 				x = x * 100
--- 				y = y * 100
--- 				cache[index] = {
--- 					x	= x,
--- 					y	= y,
--- 					spawn = GetTime(),
--- 					dir	= IdentifyCartCoord(x, y),
--- 					c	= (name:match("Red") and 0) or (name:match("Blue") and 1) or -1
--- 				}
--- 		else
-
--- 		end
-
--- 	end
--- 	local time = GetTime()
--- 	local prune = #cache < #carts
--- 	for _, newCart in pairs(cache) do
--- 		for i, cart in pairs(carts) do
--- 			if (cart.x == -1 or cart.y == -1) and cart.spawn + 1 < time then
--- 				IdentifyCart(i)
--- 				-- print(225,cartTimer:IsStarted(names[cart.dir]),names[cart.dir])
--- 				if not cartTimer:IsStarted(names[cart.dir]) then -- Prevent duplicate cart timers.
--- 					cartTimer:Start(cart.spawn + times[cart.dir] - time, names[cart.dir])
--- 				end
--- 			elseif GetDistance(newCart.x, newCart.y, cart.x, cart.y) < 1 and IsValidUpdate(cart.dir, newCart.dir) then
--- 				if newCart.c ~= cart.c then
--- 					local name = names[cart.dir]
--- 					-- print(232,cartTimer:IsStarted(names[cart.dir]),names[cart.dir])
--- 					if newCart.c == 1 then
--- 						cartTimer:SetColor({r=0, g=0, b=1}, name) -- c == 1 == Blue
--- 						cartTimer:UpdateIcon("Interface\\Icons\\INV_BannerPVP_02", name) -- Interface\\Icons\\INV_BannerPVP_02.blp
--- 					elseif newCart.c == 0 then
--- 						cartTimer:SetColor({r=1, g=0, b=0}, name) -- c == 0 == red
--- 						cartTimer:UpdateIcon("Interface\\Icons\\INV_BannerPVP_01", name) -- Interface\\Icons\\INV_BannerPVP_01.blp
--- 					else
--- 						cartTimer:SetColor({r=128, g = 128, b=128})
--- 						cartTimer:UpdateIcon("Interface\\Icons\\INV_BannerPVP_03", name) -- Interface\\Icons\\INV_BannerPVP_03.blp
--- 					end
--- 				end
--- 				cart.dir	= newCart.dir
--- 				cart.x		= newCart.x
--- 				cart.y		= newCart.y
--- 				cart.c		= newCart.c
--- 			elseif prune and (cart.spawn + times[cart.dir] - time < -1) then
--- 				carts[i] = nil
--- 			end
--- 		end
--- 	end
--- end
 local function GetCartDirByIndex(index)
 	if index == 1 then
 		return topDown and 1 or 2
@@ -244,7 +101,6 @@ local function GetCartDirByIndex(index)
 		return lavaDown and 4 or 5
 	end
 end
-
 
 function mod:UpdateCarByIndex(index)
 	if not battleStart then return end
@@ -308,7 +164,6 @@ end
 
 function mod:UpdateCartsInfoTicker()
 	self:UpdateCartsInfo()
-	-- self:UnscheduleMethod("UpdateCartsInfoTicker")
 end
 
 function mod:UpdateCartsTime()
@@ -357,6 +212,10 @@ function mod:UpdateCartsTime()
 
 end
 
+mod.CHAT_MSG_SYSTEM = mod.CHAT_MSG_RAID_BOSS_EMOTE
+mod.CHAT_MSG_BG_SYSTEM_ALLIANCE = mod.CHAT_MSG_RAID_BOSS_EMOTE
+mod.CHAT_MSG_BG_SYSTEM_HORDE = mod.CHAT_MSG_RAID_BOSS_EMOTE
+mod.CHAT_MSG_BG_SYSTEM_NEUTRAL = mod.CHAT_MSG_RAID_BOSS_EMOTE
 
 function mod:OnInitialize()
 
@@ -372,14 +231,14 @@ function mod:OnInitialize()
 				end
 			end
 		end)
-		-- cartCount = 0
+
 	elseif bgzone then
 		bgzone = false
 		battleStart = false
 		mod.updFrame:SetScript("OnUpdate",function()
 
 		end)
-		-- self:UnscheduleMethod("UpdateCartsInfoTicker")
+
 	end
 end
 
