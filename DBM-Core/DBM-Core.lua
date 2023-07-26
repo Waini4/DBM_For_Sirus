dc4f5bef2816c6656a614e896569229f60b1a596
Waini4 <65820714+Waini4@users.noreply.github.com>
2023-07-26 18:45:53 +0300

    Ап

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -81,9 +81,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("2023" .. "07" .. "20" .. "19" .. "12" .. "00"),
+	Revision = parseCurseDate("2023" .. "07" .. "26" .. "18" .. "45" .. "00"),
 	DisplayVersion = GetAddOnMetadata(_addonname, "Version"), -- the string that is shown as version
-	ReleaseRevision = releaseDate(2023, 07, 20, 19, 12, 00) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2023, 07, 26, 18, 45, 00) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
