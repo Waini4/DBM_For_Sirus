edf3c34023e180b8a86425829f81ddcb3baa8b0e
Waini4 <65820714+Waini4@users.noreply.github.com>
2024-05-12 16:30:26 +0300

    BT TEST - Teron, Souls :) + up Vers

--- DBM-BlackTemple/EssenceOfSouls.lua
+++ DBM-BlackTemple/EssenceOfSouls.lua
@@ -10,18 +10,18 @@ mod:SetUsedIcons(4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED ",
-	"SPELL_AURA_REMOVED ",
-	"SPELL_CAST_START ",
-	"SPELL_CAST_SUCCESS ",
-	"SPELL_DAMAGE ",
-	"SPELL_MISSED ",
+	"SPELL_AURA_APPLIED 371985 371999 371989 371997",
+	"SPELL_AURA_APPLIED_DOSE 371989 371997",
+	"SPELL_AURA_REMOVED",
+	"SPELL_CAST_START 371993 371992",
+	"SPELL_CAST_SUCCESS 371984 371999 371997 371994",
+	"SPELL_DAMAGE 41545",
+	"SPELL_MISSED 41545",
 	"CHAT_MSG_MONSTER_YELL",
+	"UNIT_DIED",
 	"UNIT_SPELLCAST_SUCCEEDED"
 )
-
---Реликварий
-
+--[[
 --maybe a warning for Seethe if tanks mess up in phase 3
 local warnFixate		= mod:NewTargetNoFilterAnnounce(41294, 3, nil, "Tank|Healer", 2)
 local warnDrain			= mod:NewTargetNoFilterAnnounce(41303, 3, nil, "Healer", 2)
@@ -29,19 +29,52 @@ local warnFrenzy		= mod:NewSpellAnnounce(41305, 3, nil, "Tank|Healer", 2)
 local warnFrenzySoon	= mod:NewPreWarnAnnounce(41305, 5, 2)
 local warnFrenzyEnd		= mod:NewEndAnnounce(41305, 1, nil, "Tank|Healer", 2)
 
+local specWarnPhase 	= mod:NewSpecialWarning("Скоро фаза %s", nil, nil, nil, 1, 2)
+local warnPhaseDesires  = mod:NewPhaseChangeAnnounce(2, nil, nil, nil, nil, nil, 2, 1)
 local warnPhase2		= mod:NewPhaseAnnounce(2, 2)
 local warnMana			= mod:NewAnnounce("WarnMana", 4, 41350)
 local warnDeaden		= mod:NewTargetNoFilterAnnounce(41410, 1)
-local specWarnShock		= mod:NewSpecialWarningInterrupt(41426, "HasInterrupt", nil, 2)
+]]
+local specWarnPhase 	= mod:NewSpecialWarning("Скоро фаза %s", nil, nil, nil, 1, 2)
+local Stage2             	= mod:NewPhaseTimer(11, nil, "Фаза: %s", nil, nil, 4)
 
+-- Гнева
+local warnRage				= mod:NewTargetAnnounce(371999, 3)
+local warnGripStacks		= mod:NewStackAnnounce(371997, 2, nil, "Tank")
+local specWarnRageDispel	= mod:NewSpecialWarningDispel(371999, "RemoveCurse", nil, nil, 1, 2)
+
+local specWarnKick			= mod:NewSpecialWarningInterrupt(371993, "HasInterrupt", nil, nil, 1, 2)
+
+local timerNextGrip 		= mod:NewNextTimer(3, 371997, nil, nil, nil, 5)
+--local timerGrip				= mod:NewTargetTimer(10, 371997, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerNextRage 		= mod:NewCDTimer(20, 371999, nil, nil, nil, 5)
+
+--желания
+local specWarnThirst        = mod:NewSpecialWarningMoveAway(371992, "Melee", nil, nil, 4, 2) --Пожирающая жажда
+local specWarnDeceit		= mod:NewSpecialWarningStack(371989, nil, 10, nil, nil, 1, 6)
+
+local timerNextThirst		= mod:NewCDTimer(18, 371992, nil, nil, nil, 5)
+local timerNextKick		= mod:NewCDTimer(10, 371993, nil, nil, nil, 5) --желания
+--Воплощение страдания
+
+ --371985,"Иссушающая лихорадка"
+ local warnFever		= mod:NewTargetAnnounce(371985, 3)
+ local specWarnGTFO	    = mod:NewSpecialWarningGTFO(371984, nil, nil, nil, 1, 2)
+
+ local timerNextGTFO		= mod:NewCDTimer(9, 371984, nil, nil, nil, 3) --реликв потерь
+
+--[[
 local warnPhase3		= mod:NewPhaseAnnounce(3, 2)
 local warnSoul			= mod:NewSpellAnnounce(41545, 2, nil, "Tank", 2)
 local warnSpite			= mod:NewTargetAnnounce(41376, 3)
 
-local specWarnShield	= mod:NewSpecialWarningDispel(41431, "MagicDispeller", nil, 2, 1, 2)
+local specWarnShield	= mod:NewSpecialWarningDispel(41431, "RemoveCurse", nil, 2, 1, 2)
 local specWarnSpite		= mod:NewSpecialWarningYou(41376, nil, nil, nil, 1, 2)
+local specWarnGTFO	    = mod:NewSpecialWarningGTFO(371984, nil, nil, nil, 1, 2)
 
 --Phase 1
+local timerNextGTFO		= mod:NewCDTimer(9, 371984, nil, nil, nil, 3) --реликв потерь
+
 local timerPhaseChange	= mod:NewPhaseTimer(41)
 local timerFrenzy		= mod:NewBuffActiveTimer(8, 41305, nil, "Tank|Healer", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerNextFrenzy	= mod:NewNextTimer(40, 41305, nil, "Tank|Healer", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
@@ -56,78 +89,116 @@ local timerNextSoul		= mod:NewCDTimer(10, 41545, nil, "Tank", 2, 5, nil, DBM_COM
 
 mod:AddSetIconOption("DrainIcon", 41303, false)
 mod:AddSetIconOption("SpiteIcon", 41376, false)
-
+]]
+local GripBuff = DBM:GetSpellInfoNew(371997)
 mod.vb.lastFixate = "None"
+local FeverTargets = {}
+local RageTargets = {}
+local UnitSpirits = 0
+mod.vb.StageUwU = 0
+mod:AddInfoFrameOption(371997, true)
+local Stages = { "Воплощение Гнева", "Воплощение Желания", "Воплощение Страдания"}
+
+--[[
+0x80000000,371980,"Аура ужасных страданий",0x20
+	371980,"Аура ужасных страданий",0x20,DEBUFF
+
+	23:07:35.615 - 23:08:54.826 = 25+54=79
+
+23:24:53.069 - 23:26:24.904 = 2.29	1.28
+23:30:14.210 - 23:30:25.141 = 0.01
+
+23:35:41.702 - 23:37:14.248 = 1.27 60+32=92
+23:39:02.203 - 23:39:22.263 = 0.20
+
+23:42:36.957 - 23:43:54.061 = 67
+
+
+]]
+
+local function warnFeverTargets(self)
+	warnFever:Show(table.concat(FeverTargets, "<, >"))
+	table.wipe(FeverTargets)
+end
+local function warnRageTargets(self)
+	warnRage:Show(table.concat(RageTargets, "<, >"))
+	table.wipe(RageTargets)
+end
+
+local function Unit(self)
+	self.vb.StageUwU = 0
+end
+
+
+
 
 function mod:OnCombatStart(delay)
-	self.vb.lastFixate = "None"
-	timerNextFrenzy:Start(49-delay)
-	warnFrenzySoon:Schedule(44-delay)
+	UnitSpirits = 0
+	self.vb.StageUwU = 1
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 41305 then
-		warnFrenzy:Show()
-		timerFrenzy:Start()
-	elseif args.spellId == 41431 and not args:IsDestTypePlayer() then
-		timerNextShield:Start()
-		specWarnShield:Show(args.destName)
-		specWarnShield:Play("dispelboss")
-	elseif args.spellId == 41376 then
-		warnSpite:CombinedShow(0.3, args.destName)
+	if args.spellId == 371985 then
+		FeverTargets[#FeverTargets + 1] = args.destName
+		self:Unschedule(warnFeverTargets)
+		self:Schedule(0.1, warnFeverTargets, self)
+	elseif args.spellId == 371999 then
+		RageTargets[#RageTargets + 1] = args.destName
+		self:Unschedule(warnRageTargets)
+		self:Schedule(0.1, warnRageTargets, self)
+	elseif args.spellId == 371997 then
+		local amount = args.amount or 1
+		warnGripStacks:Show(args.destName, amount)
+		--timerGrip:Start()
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(GripBuff)
+			DBM.InfoFrame:Show(30, "playerdebuffstacks", GripBuff, 2)
+		end
+	elseif args.spellId == 371989 then
 		if args:IsPlayer() then
-			specWarnSpite:Show()
-			specWarnSpite:Play("defensive")
+			if ((args.amount or 1) >= 10) and self:AntiSpam(4, 2) then
+				specWarnDeceit:Show(args.amount)
+			end
 		end
-		if self.Options.SpiteIcon then
-			self:SetAlphaIcon(0.5, args.destName)
-		end
-	elseif args.spellId == 41303 then
-		warnDrain:CombinedShow(1, args.destName)
-		if self.Options.DrainIcon then
-			self:SetAlphaIcon(1, args.destName)
-		end
-	elseif args.spellId == 41294 then
-		if self.vb.lastFixate ~= args.destName then
-			warnFixate:Show(args.destName)
-			self.vb.lastFixate = args.destName
-		end
-	elseif args.spellId == 41410 and not args:IsDestTypePlayer() then
-		warnDeaden:Show(args.destName)
-		timerDeaden:Start(args.destName)
 	end
 end
 
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 41305 then
 		warnFrenzyEnd:Show()
 		warnFrenzySoon:Schedule(35)
 		timerNextFrenzy:Start()
 	end
-end
+end]]
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 41410 then
-		timerNextDeaden:Start()
-	elseif args.spellId == 41426 then
-		timerNextShock:Start()
-		specWarnShock:Show(args.sourceName)
+	if args.spellId == 371992 then
+		specWarnThirst:Show(args.destName)
+		timerNextThirst:Start()
+	elseif args.spellId == 371993 and self:AntiSpam(3, 1) then
+		timerNextKick:Start()
+		specWarnKick:Show(args.sourceName)
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 41350 then --Aura of Desire
-		warnPhase2:Show()
-		warnMana:Schedule(130)
-		timerMana:Start()
-		timerNextShield:Start(13)
-		timerNextDeaden:Start(28)
-	elseif args.spellId == 41337 then --Aura of Anger
-		warnPhase3:Show()
-		timerNextSoul:Start()
+	if args.spellId == 371984 then --Aura of Desire
+		timerNextGTFO:Start()
+		specWarnGTFO:Show(args.sourceName)
+	elseif args.spellId == 371999 and self:AntiSpam(3, 1) then --Aura of Anger
+		specWarnRageDispel:Show(args.sourceName)
+		timerNextRage:Start()
+	elseif args.spellId == 371997 then
+		timerNextGrip:Start()
+	elseif args.spellId == 371980 then
+		self.vb.StageUwU = 0
 	end
 end
 
+--[[
 function mod:SPELL_DAMAGE(_, _, _, _, _, _, spellId)
 	if spellId == 41545 and self:AntiSpam(3, 1) then
 		warnSoul:Show()
@@ -165,3 +236,17 @@ function mod:OnSync(msg)
 		timerPhaseChange:Start()--41
 	end
 end
+]]
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 23469 then
+		UnitSpirits = UnitSpirits+1
+		if UnitSpirits == 8 then
+			self.vb.StageUwU = self.vb.StageUwU+1
+			Stage2:Start(nil, Stages[self.vb.StageUwU])
+			specWarnPhase:Show(Stages[self.vb.StageUwU])
+			self:Schedule(2, Unit, self)
+		end
+	end
+end -- Гнев, желания,
--- DBM-BlackTemple/TeronGorefiend.lua
+++ DBM-BlackTemple/TeronGorefiend.lua
@@ -10,63 +10,106 @@ mod:SetUsedIcons(4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED ",
+	"SPELL_CAST_START 373799 373791 373807 373795",
+	"SPELL_AURA_APPLIED 373795 373811",
+	"SPELL_AURA_APPLIED_DOSE 373795 373811",
 	"SPELL_AURA_REMOVED ",
-	"SPELL_CAST_SUCCESS "
+	"SPELL_CAST_SUCCESS 373803 373796"
 )
 
---Incinerate useful?
-local warnCrushed			= mod:NewTargetNoFilterAnnounce(40243, 3, nil, "Healer")
-local warnIncinerate		= mod:NewSpellAnnounce(40239, 3)
-local warnDeath				= mod:NewTargetNoFilterAnnounce(40251, 3)
+mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(1) .. " : " .. "|cff00f7ffВласть Белого Хлада|r")
+local warnFreezingStacks	= mod:NewStackAnnounce(373795, 2, nil, "Tank")
+local specWarnCold			= mod:NewSpecialWarningGTFO(373796, "SpellCaster", nil, nil, 4, 2)
 
-local specWarnDeath			= mod:NewSpecialWarningYou(40251, nil, nil, nil, 1, 2)
-local specWarnDeathEnding	= mod:NewSpecialWarningMoveAway(40251, nil, nil, nil, 3, 2)
+local timerNextFreezing		= mod:NewCDTimer(6, 373795, nil, "Tank", nil, 3)
+local timerColdCast 		= mod:NewCastTimer(10, 373798, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerCold     		= mod:NewCDTimer(20, 373796, nil, "SpellCaster", nil, 4, nil, DBM_COMMON_L.DEADLY_ICON, nil, 1)
+local StageTimer			= mod:NewPhaseTimer(120, nil, "Скоро Фаза: %s", nil, nil, 4)
 
-local timerCrushed			= mod:NewBuffActiveTimer(15, 40243, nil, "Healer", 2, 5, nil, DBM_COMMON_L.HEALER_ICON)
-local timerDeathCD			= mod:NewCDTimer(32, 40251, nil, nil, nil, 3)--32-40 (small sample size, could be bigger range)
-local timerDeath			= mod:NewTargetTimer(55, 40251, nil, nil, nil, 3)
-local timerVengefulSpirit	= mod:NewTimer(60, "TimerVengefulSpirit", 40325, nil, nil, 1)
+local FreezBuff 			= DBM:GetSpellInfoNew(373795)
 
-mod:AddSetIconOption("CrushIcon", 40243, false, false, {1, 2, 3, 4, 5})
+mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(2) .. " : " .. "|cffff1919Власть Страданий|r")
 
-local CrushedTargets = {}
-mod.vb.crushIcon = 1
+--local warnReposeStacks	= mod:NewStackAnnounce(373811, 2, nil, "Tank")
+local specWarnDecapitation	= mod:NewSpecialWarningDodge(373803, nil, nil, nil, 3, 2)
 
-local function showCrushedTargets(self)
-	warnCrushed:Show(table.concat(CrushedTargets, "<, >"))
-	table.wipe(CrushedTargets)
-	self.vb.crushIcon = 1
-end
+local timerDecapitation     = mod:NewCDTimer(12, 373803, nil, "Melee", nil, 5, nil, DBM_COMMON_L.DEADLY_ICON, nil, 1)
+
+
+mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(3) .. " : " .. "|cfffc9bffВласть Тьмы|r")
+
+local warnReposeStacks		= mod:NewStackAnnounce(373811, 2, nil, "Tank")
+
+local timerNextRepose		= mod:NewCDTimer(6, 373811, nil, "Tank", nil, 3)
+local berserkTimer          = mod:NewBerserkTimer(90)
+--local timerRepos			= mod:NewTimer(60, "Стаки: %d, %s", 373811, "Tank", nil, 3) -- Прим удар
+
+local ReposeBuff = DBM:GetSpellInfoNew(373811)
+mod:AddRangeFrameOption(9, nil, true)
+local StageAura = {"Власть Страданий", "Власть Тьмы"}
+--local CrushedTargets = {}
+mod.vb.Aura = 0
+mod:AddInfoFrameOption(373795, true)
+
 
 function mod:OnCombatStart(delay)
-	table.wipe(CrushedTargets)
-	timerDeathCD:Start(11.1)--11-13?
+	self.vb.Aura = 0
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show(9)
+	end
+end
+
+function mod:OnCombatEnd(wipe)
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Hide()
+	elseif self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 373799 then
+		specWarnDecapitation:Schedule(10)
+		timerDecapitation:Start()
+		StageTimer:Start(nil, StageAura[self.vb.Aura+1])
+		timerCold:Stop()
+		timerNextFreezing:Stop()
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:Hide()
+		end
+	elseif args.spellId == 373795 then
+		timerNextFreezing:Start()
+	elseif args.spellId == 373791 then
+		timerCold:Start()
+		StageTimer:Start(nil, StageAura[self.vb.Aura+1])
+	elseif args.spellId == 373807 then
+		timerNextRepose:Start()
+		timerDecapitation:Stop()
+		specWarnDecapitation:Cancel()
+		berserkTimer:Start()
+	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 40243 then
-		CrushedTargets[#CrushedTargets + 1] = args.destName
-		timerCrushed:Start()
-		self:Unschedule(showCrushedTargets)
-		if self.Options.CrushIcon then
-			self:SetIcon(args.destName, self.vb.crushIcon, 15)
-			self.vb.crushIcon = self.vb.crushIcon + 1
+	local amount = args.amount or 1
+	if args.spellId == 373811 then
+		warnReposeStacks:Show(args.destName, amount)
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(ReposeBuff)
+			DBM.InfoFrame:Show(30, "playerdebuffstacks", ReposeBuff, 2)
 		end
-		self:Schedule(0.5, showCrushedTargets, self)
-	elseif args.spellId == 40251 then
-		timerDeath:Start(args.destName)
-		if args:IsPlayer() then
-			specWarnDeath:Show()
-			specWarnDeath:Play("targetyou")
-			specWarnDeathEnding:Schedule(50)
-			specWarnDeathEnding:ScheduleVoice(50, "runout")
-		else
-			warnDeath:Show(args.destName)
+	elseif args.spellId == 373795 then
+		warnFreezingStacks:Show(args.destName, amount)
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(FreezBuff)
+			DBM.InfoFrame:Show(30, "playerdebuffstacks", FreezBuff, 2)
 		end
 	end
 end
 
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 40243 then
 		if self.Options.CrushIcon then
@@ -80,12 +123,15 @@ function mod:SPELL_AURA_REMOVED(args)
 			specWarnDeathEnding:CancelVoice()
 		end
 	end
-end
+end]]
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 40239 then
-		warnIncinerate:Show()
-	elseif args.spellId == 40251 then
-		timerDeathCD:Start()
+	if args.spellId == 373803 then
+		specWarnDecapitation:Schedule(10)
+		timerDecapitation:Start()
+	elseif args.spellId == 373796 then
+		timerCold:Start()
+		timerColdCast:Start()
+		specWarnCold:Show(args.SourceName)
 	end
 end
