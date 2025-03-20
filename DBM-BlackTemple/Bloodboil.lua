8f29a6f8c694a32b8f0ab9e5f725de6f1caabcce
Waini4 <65820714+Waini4@users.noreply.github.com>
2025-03-20 07:09:28 +0200

    Up ver, BT- Ork (add announce cast entropy)

--- DBM-BlackTemple/Bloodboil.lua
+++ DBM-BlackTemple/Bloodboil.lua
@@ -10,7 +10,7 @@ mod:SetModelID(22948)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 373745",
+	"SPELL_CAST_START 373745 373748",
 	"SPELL_CAST_SUCCESS 42005",
 	"SPELL_AURA_APPLIED 373742 373744 373749 373747",
 	"SPELL_AURA_APPLIED_DOSE 373742 373749 373747",
@@ -21,19 +21,22 @@ mod:RegisterEventsInCombat(
 --local warnFilth			= mod:NewTargetAnnounce(373742, 3)
 
 --local specWarnGTFO		= mod:NewSpecialWarningGTFO(373744, nil, nil, nil, 4, 8)
-local specWarnFilth   = mod:NewSpecialWarningStack(373742, nil, 18, nil, nil, 2, 2)
-local specWarnBlood   = mod:NewSpecialWarningStack(373749, nil, 2, nil, nil, 1, 2)
---local specWarnRage		= mod:NewSpecialWarningYou(40604, nil, nil, nil, 1, 2)
-local yellFilth       = mod:NewCountYell(373742)
 
-local timerStrikeCast = mod:NewCastTimer(1.5, 373745, nil, "Tank", nil, 2)
-local timerStrikeCD   = mod:NewCDTimer(6, 373745, nil, "Tank", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
+local specWanrWaveCast = mod:NewSpecialWarningCast(373748, nil, nil, nil, 3)
+local specWarnFilth    = mod:NewSpecialWarningStack(373742, nil, 18, nil, nil, 2, 2)
+local specWarnBlood    = mod:NewSpecialWarningStack(373749, nil, 2, nil, nil, 1, 2)
+--local specWarnRage		= mod:NewSpecialWarningYou(40604, nil, nil, nil, 1, 2)
+local yellFilth        = mod:NewCountYell(373742)
+
+local timerWaveCast    = mod:NewCastTimer(3, 373748, nil, nil, nil, 3, nil, DBM_COMMON_L.IMPORTANT_ICON)
+local timerStrikeCast  = mod:NewCastTimer(1.5, 373745, nil, "Tank", nil, 2)
+local timerStrikeCD    = mod:NewCDTimer(6, 373745, nil, "Tank", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 --local berserkTimer		= mod:NewBerserkTimer(600)
 
 --mod:AddSetIconOption("SetIconOnFilth", 373742, true, true, { 2, 3, 4, 5, 6, 7, 8})
-mod:AddInfoFrameOption(373742)
 --mod:AddInfoFrameOption(373747)
+mod:AddInfoFrameOption(373742)
 --local ApofStack = DBM:GetSpellInfoNew(373747)
 local FilthBuff = DBM:GetSpellInfoNew(373742)
 mod:AddRangeFrameOption(8, nil, true)
@@ -65,6 +68,9 @@ function mod:SPELL_CAST_START(args)
 	if args.spellId == 373745 then
 		timerStrikeCast:Start()
 		timerStrikeCD:Start()
+	elseif args.spellId == 373748 then
+		specWanrWaveCast:Show()
+		timerWaveCast:Start()
 	end
 end
 
